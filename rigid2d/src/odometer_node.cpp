a02d1eecaf651d8ef1ea1fea01bb5d0966d43310
Maurice Rahme <mrahme97@gmail.com>
2020-01-25 16:57:14 -0600

    tried to modify xacro, still teleports...

--- rigid2d/src/odometer_node.cpp
+++ rigid2d/src/odometer_node.cpp
@@ -23,6 +23,7 @@ rigid2d::Twist2D Vb;
 rigid2d::WheelVelocities w_vel;
 rigid2d::DiffDrive driver;
 rigid2d::Pose2D pose;
+bool callback_flag = false;
 
 void js_callback(const sensor_msgs::JointState::ConstPtr &js)
 {
@@ -35,6 +36,7 @@ void js_callback(const sensor_msgs::JointState::ConstPtr &js)
 	pose = driver.get_pose();
   // Print Wheel Angles
 	// std::cout << driver;
+  callback_flag = true;
 }
 
 int main(int argc, char** argv)
@@ -48,7 +50,7 @@ int main(int argc, char** argv)
   ros::NodeHandle nh("~"); // get a handle to ROS
   // Init Private Parameters
   nh.param<std::string>("odom_frame_id", o_fid_, "odom");
-  nh.param<std::string>("body_frame_id", b_fid_, "base_footprint");
+  nh.param<std::string>("body_frame_id", b_fid_, "base_link");
   nh.param<std::string>("left_wheel_joint", wl_fid_, "left_wheel_axle");
   nh.param<std::string>("right_wheel_joint", wr_fid_, "right_wheel_axle");
   nh.param<float>("/wheel_base", wbase_, 1.5);
@@ -78,36 +80,41 @@ int main(int argc, char** argv)
 
   	// Update and Publish Odom Transform
   	// Init Tf
-  	geometry_msgs::TransformStamped odom_tf;
-    odom_tf.header.stamp = current_time;
-    odom_tf.header.frame_id = o_fid_;
-    odom_tf.child_frame_id = b_fid_;
-  	// Pose
-  	odom_tf.transform.translation.x = pose.x;
-  	odom_tf.transform.translation.y = pose.y;
-  	odom_tf.transform.translation.z = 0;
-  	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose.theta);
-  	odom_tf.transform.rotation = odom_quat;
-  	// Send the Transform
-    odom_broadcaster.sendTransform(odom_tf);
+    if (callback_flag == true)
+    {
+      geometry_msgs::TransformStamped odom_tf;
+      odom_tf.header.stamp = current_time;
+      odom_tf.header.frame_id = o_fid_;
+      odom_tf.child_frame_id = b_fid_;
+      // Pose
+      odom_tf.transform.translation.x = pose.x;
+      odom_tf.transform.translation.y = pose.y;
+      odom_tf.transform.translation.z = 0;
+      geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pose.theta);
+      odom_tf.transform.rotation = odom_quat;
+      // Send the Transform
+      odom_broadcaster.sendTransform(odom_tf);
 
-    // Update and Publish Odom Msg
-    // Init Msg
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = o_fid_;
-    // Pose
-    odom.pose.pose.position.x = pose.x;
-    odom.pose.pose.position.y = pose.y;
-    odom.pose.pose.position.z = 0.0;
-    odom.pose.pose.orientation = odom_quat;
-    // Twist
-    odom.child_frame_id = b_fid_;
-    odom.twist.twist.linear.x = Vb.v_x;
-    odom.twist.twist.linear.y = Vb.v_y;
-    odom.twist.twist.angular.z = Vb.w_z;
-    // Publish the Message
-    odom_pub.publish(odom);
+      // Update and Publish Odom Msg
+      // Init Msg
+      nav_msgs::Odometry odom;
+      odom.header.stamp = current_time;
+      odom.header.frame_id = o_fid_;
+      // Pose
+      odom.pose.pose.position.x = pose.x;
+      odom.pose.pose.position.y = pose.y;
+      odom.pose.pose.position.z = 0.0;
+      odom.pose.pose.orientation = odom_quat;
+      // Twist
+      odom.child_frame_id = b_fid_;
+      odom.twist.twist.linear.x = Vb.v_x;
+      odom.twist.twist.linear.y = Vb.v_y;
+      odom.twist.twist.angular.z = Vb.w_z;
+      // Publish the Message
+      odom_pub.publish(odom);
+
+      callback_flag = false;
+    }
 
     rate.sleep();
   }
